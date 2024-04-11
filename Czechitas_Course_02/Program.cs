72cf4a7d1e17ad316933cd8fc3c6a00087ae2d5e
Kristýna Sekničková <117662870+Nugetinka@users.noreply.github.com>
2024-04-11 19:46:37 +0200

    Add Lekce_01

---
+++ Czechitas_Course_02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lekce_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, vítej na kurzu Czechitas 2 :D");
+        }
+    }
+}
