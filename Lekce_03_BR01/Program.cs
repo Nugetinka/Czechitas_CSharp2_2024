9b5f46097523ded71753111e3645bdd8cc4f12f1
Kristýna Sekničková <117662870+Nugetinka@users.noreply.github.com>
2024-04-28 17:59:42 +0200

    Lekce 03
    Vše z lekce 3, včetně úkolu.

---
+++ Lekce_03_BR01/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Lekce_03_BR01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ukol - Obratte poradi stringu
+            string palindrom = "Kuna nese nanuk"; //15 charakteru
+            string obracenyString = "";
+            for (int i = palindrom.Length; i > 0; i--)
+            {
+                //Console.WriteLine(palindrom[i - 1]);
+                obracenyString += palindrom[i - 1];
+            }
+
+            Console.WriteLine(obracenyString);
+
+            int[] pole = new[] { 4, 5, 6 };
+            int soucet = 0;
+            for (int i = 0; i < pole.Length; i++)
+            {
+                soucet += pole[i];
+                //index zacina nulou
+            }
+
+            //Ukol - Napiste funkci, ktera umi detekovat, ze se jedna o palindrom (zatim jen u slov) a pak z pole vypiste jen palindromy
+            string[] slova = new string[] { "kajak", "program", "rotor", "Czechitas", "madam", "Jarda", "radar", "nepotopen" };
+
+            static bool JePalindrom(string slovo)
+            {
+                // cyklus by se mohl optimalizovat tak, ze bychom meli jen polovicni cislo cyklu -> slovo.Length/2
+                for (int i = 0; i < slovo.Length; i++)
+                {
+                    if (slovo[i] != slovo[slovo.Length - 1 - i])
+                    {
+                        //pokud prvni a posledni charakter ve stringu se nerovna, tak uz vime ze to neni palindrom
+                        //tim padem pokud vratime return false, tak se for loop cyklus uz predcasne ukonci a nebude dal pokracovat v dalsich cyklech
+                        return false;
+                    }
+                }
+                //Tim ze for cyklus dobehl jsme si jisti ze slovo je palindrom
+
+                return true;
+            }
+
+            for (int i = 0; i < slova.Length; i++)
+            {
+                bool jeSlovoPalindrom = JePalindrom(slova[i]);
+                if (jeSlovoPalindrom)
+                {
+                    Console.WriteLine(slova[i]);
+                }
+            }
+
+
+
+
+            //Ukol - opravte v tomto textu omylem zapnuty Caps Lock
+            string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
+
+            //Ukol - rozsifrujte tuto zpravu - text byl zasifrovan tak, ze jsme kazde pismeno posunuli o jedno doprava: 'a' -> 'b'. 
+            string sifra = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
+
+        }
+    }
+}
---
+++ Lekce_03_Text/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Text;
+
+namespace Lekce_03_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------");
+            string oddelovac = new string('-', 20);
+            Console.WriteLine(oddelovac);
+
+            char maleAcko = 'a';
+            char velkeAcko = 'A';
+            Console.WriteLine(maleAcko + velkeAcko);
+            int ciselnaHodnotaMaleAcko = (int)maleAcko;
+            int ciselnaHodnotaVelkeAcko = (int)velkeAcko;
+            Console.WriteLine(ciselnaHodnotaMaleAcko);
+            Console.WriteLine(ciselnaHodnotaVelkeAcko);
+
+            for (int i = 0; i < 127; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(" ");
+                Console.WriteLine((char)i);
+            }
+
+            int vychoziInt = default(int);
+            string vychoziString = default(string);
+            Console.WriteLine(vychoziInt == null);
+            Console.WriteLine(vychoziString == null);
+
+            string vstup = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(vstup))
+            {
+                Console.WriteLine("Špatný vstup");
+                vstup = Console.ReadLine();
+            }
+
+            Console.WriteLine($"malé a vypadá takto: {maleAcko}");
+
+            string uzivatelskeJmeno = "Kristýna";
+            string heslo = "Czechitas";
+            Console.WriteLine("Zadej jmeno: ");
+            string vstupJmeno = Console.ReadLine();
+            string vstupOcisteny = vstupJmeno.Trim();
+            Console.WriteLine($"Vstup původní: [{vstupJmeno}] a po očištění [{vstupOcisteny}]");
+            if (string.Equals(uzivatelskeJmeno, vstupOcisteny, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Zadej heslo: ");
+                string vstupHeslo = Console.ReadLine();
+                if (vstupHeslo == heslo)
+                {
+                    Console.WriteLine("Vítej, vše v pořádku.");
+                }
+            }
+
+            string prvniString = "aa";
+            string druhyString = "ab";
+            int vysledekPorovnani = string.Compare(prvniString, druhyString, StringComparison.Ordinal);
+            Console.WriteLine($"Výsledek porovnání: {vysledekPorovnani}");
+
+            string dlouhyText = "Ahoj Pepo, jak se máš? Ahoj.";
+            bool obsahujePepu = dlouhyText.Contains("pepo", StringComparison.OrdinalIgnoreCase);
+            int indexPepa = dlouhyText.IndexOf("Pepo");
+            Console.WriteLine($"Obsahuje pepu: {obsahujePepu} na {indexPepa} pozici");
+
+            string prvnich10znaku = dlouhyText.Substring(startIndex: 0, length: 10);
+            Console.WriteLine($"Prvních 10 znaků: {prvnich10znaku}");
+            string odPepy = dlouhyText.Substring(startIndex: indexPepa);
+            Console.WriteLine($"Znaky od Pepy {odPepy}");
+
+            string pozdravHonzy = dlouhyText.Replace("Pepo", "Honzo");
+            Console.WriteLine($"Pozdrav Honzy: {pozdravHonzy}");
+
+            string pozdravPepyCau = dlouhyText.Replace("Ahoj", "Čau");
+            Console.WriteLine($"Pozdrav s Čau: {pozdravPepyCau}");
+
+            string jidlaVcelku = "rizek, kedlubna";
+            string[] jidla = jidlaVcelku.Split(',');
+            //string[] jidla = { "rizek", "kedlubna" };
+            for (int i = 0; i < jidla.Length; i++)
+            {
+                Console.Write(jidla[i].PadRight(20, '.'));
+            }
+
+
+           // Vytvoření přes SB
+           StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 1000; i++)
+            {
+                sb.Append(i);
+            }
+            Console.WriteLine(sb.ToString());
+
+            // Vytvoření bez SB
+            string souhrn = "";
+            for (int i = 0; i < 1000; i++)
+            {
+                souhrn += i;
+            }
+            Console.WriteLine(souhrn);
+        }
+    }
+}
