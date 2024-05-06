3c2da5122fab2b43a4fb7f305ad4e1ca881d3d6b
Kristýna Sekničková <117662870+Nugetinka@users.noreply.github.com>
2024-05-06 22:18:26 +0200

    Fix úkolů z Lekce 02

---
+++ Lekce_02_Ukol01/Kalkulacka.cs
@@ -0,0 +1,33 @@
+﻿namespace Lekce_02_Ukol01
+{
+    class Kalkulacka
+    {
+        public double vysledek;
+        public double desetinneCislo;
+
+        public double Secti(double a, double b)
+        {
+            vysledek = a + b;
+            return vysledek;
+        }
+
+        public double NactiDesetinneCisloZKonzole(string textOdUzivatele)
+        {
+            while (true)
+            {
+                bool jeCisloPlatne = double.TryParse(textOdUzivatele, out desetinneCislo);
+
+                if (jeCisloPlatne)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Zadal jsi neplatné číslo, zkus to znovu: ");
+                    textOdUzivatele = Console.ReadLine();
+                }
+            }
+            return desetinneCislo;
+        }
+    }
+}
