2e03a5a3b2a8cf0d9c016e3a6d8836fe7f65f51d
Kristýna Sekničková <117662870+Nugetinka@users.noreply.github.com>
2024-05-28 21:03:20 +0200

    Lekce_06_ukol

--- Lekce_06_BR01/Obdelnik.cs
+++ Lekce_06_BR01/Obdelnik.cs
@@ -2,8 +2,17 @@
 {
     public class Obdelnik
     {
-        public double Sirka;
-        public double Vyska;
+        public double Sirka { get; private set; }
+        public double Vyska { get; private set; }
+        public double Obsah 
+        { 
+            get { return Sirka * Vyska; }
+        }
+
+        public double Obvod
+        {
+            get { return 2 * (Sirka + Vyska); }
+        }
 
         public Obdelnik(double sirka, double vyska)
         {
@@ -22,13 +31,19 @@
         }
 
         public Obdelnik(double strana) : this(strana, strana)
-        { 
+        {
             // čtverec
         }
 
         public void VypisParametryObdelniku()
         {
             Console.WriteLine($"Šířka: {Sirka}, Výška: {Vyska}");
+        }
+
+        public void ZvetsiParametryObdelniku(double pridanaSirka, double pridanaVyska)
+        {
+            Sirka += pridanaSirka;
+            Vyska += pridanaVyska;
         }
     }
 }
